f8ec8f1130d82c42cb1e6634a1cfe416dbb7ce7f
hectorcoronado <hectorcoronado.developer@gmail.com>
2018-11-07 17:38:50 -0800

    installs & configures enzyme, creates rudimentary Header component tests to...test config

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+// import ReactShallowRenderer from 'react-test-renderer/shallow'
+import { shallow } from 'enzyme'
+import toJSON from 'enzyme-to-json'
+
+
+import Header from '../../components/Header'
+
+// test('should render Header correctly', () => {
+//     const renderer = new ReactShallowRenderer()
+//     renderer.render(<Header />)
+//     expect(renderer.getRenderOutput()).toMatchSnapshot()
+//     console.log(renderer.getRenderOutput())
+// })
+
+test('should render Header correctly', () => {
+    const wrapper = shallow(<Header />)
+
+    expect(wrapper.find('h1').text()).toBe('expensify')
+
+    expect(wrapper).toMatchSnapshot()
+})
