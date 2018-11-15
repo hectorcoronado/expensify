dca4e70ac37dbeb92ab97afb8afa8ba873d01548
hectorcoronado <hectorcoronado.developer@gmail.com>
2018-11-14 18:58:52 -0800

    creates tests for AddExpensePage component

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -7,20 +7,53 @@ import ExpenseForm from './ExpenseForm'
 // action generator
 import { addExpense } from '../actions/expenses'
 
-const AddExpensePage = props => (
-  <div>
-    <h1>add expense</h1>
-    {/* onSubmit is the function that is made available via props
-      * in ExpenseForm.js to submit the form (that component in turn
-      * makes the expense data available here)
-      */}
-    <ExpenseForm onSubmit={expense => {
-        props.dispatch(addExpense(expense))
-        // redirect user to homepage after adding expense
-        props.history.push('/')
-      }}
-    />
-  </div>
+export class AddExpensePage extends React.Component {
+  onSubmit = expense => {
+    this.props.onSubmit(expense)
+    this.props.history.push('/')
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>add expense</h1>
+        <ExpenseForm onSubmit={ this.onSubmit }/>
+      </div>
+    )
+  }
+}
+
+/**
+ * returns an object, wherein we define props that call dispatch
+ *    - this config makes it much easier to test this component, as
+ *      opposed to directly testing a function (`addExpense`) that we
+ *      import from elsewhere
+ */
+const mapDispatchToProps = dispatch => (
+  { onSubmit: expense => dispatch(addExpense(expense)) }
 )
 
-export default connect()(AddExpensePage)
+/**
+ * `connect()` can be called with 2 parameters:
+ *    - 1st argument would be `mapStateToProps`
+ *    - 2nd argument would be `mapDispatchToProps`
+ */
+export default connect(undefined, mapDispatchToProps)(AddExpensePage)
+
+// const AddExpensePage = props => (
+//   <div>
+//     <h1>add expense</h1>
+//     {/* onSubmit is the function that is made available via props
+//       * in ExpenseForm.js to submit the form (that component in turn
+//       * makes the expense data available here)
+//       */}
+//     <ExpenseForm 
+//       onSubmit={expense => {
+//         // props.dispatch(addExpense(expense))
+//         props.onSubmit(expense)
+//         // redirect user to homepage after adding expense
+//         props.history.push('/')
+//       }}
+//     />
+//   </div>
+// )
---
+++ src/tests/components/AddExpensePage.test.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import { AddExpensePage } from '../../components/AddExpensePage'
+import expenses from '../fixtures/expenses'
+
+let onSubmit, history, wrapper
+
+// function from jest that allows for easier config/less duplication
+beforeEach(() => {
+    onSubmit = jest.fn()
+    history = { push: jest.fn() }
+    wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />)
+
+})
+
+/**
+ * `AddExpensePage` expects 2 props:
+ *      - onSubmit
+ *      - history
+ * we can pass those in with anything as their values,
+ * so we use spies (`jest.fn()`)
+ */
+test('should render AddExpensePage correctly', () => {
+    expect(wrapper).toMatchSnapshot()
+})
+
+/**
+ * test `onSubmit` function directly, passing in data that would
+ * get passed in by a real user (an expense)
+ */
+test('should handle onSubmit', () => {
+    wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1])
+    /**
+     * after the `onSubmit` function runs:
+     *      - expect history.push (a spy) to be called with the str '/'
+     *      - expect `onSubmit` to be called with the data passed in
+     */
+    expect(history.push).toHaveBeenLastCalledWith('/')
+    expect(onSubmit).toHaveBeenLastCalledWith(expenses[1])
+})
